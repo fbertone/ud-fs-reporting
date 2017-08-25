c1d03c684790b6ff53b29498ccb23b3fed2115c3
Fabrizio Bertone <fab.bertone@gmail.com>
2017-08-25 21:26:12 +0200

    resubmission after first review

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # SQL Reporting Tool for Udacity Fullstack Nanodegree
 
-To use the tool you need a postgresql server with the Udacity Fullstack 'news' DB.
+## Description
+This Python CLI tool analyse a PostgreSQL DB and outputs a report.
+Three different analysis are executed, the objective is to use just an SQL query each,
+exploiting table joins and subqueries.
 
-Tested with Python 2.7
+## Set up
+* Install PostgreSQL Server
+* Download the data [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
+* Extract the file newsdata.sql from the zip file
+* Load the data into the database with the following command:
 
-To tun the reporting tool, use the command:
+> psql -d news -f newsdata.sql
 
+## Running the reporting tool
+The tool was tested with with Python v. 2.7.
+
+You can run the tool calling the python interpreter:
 > python reporting.py
+or launching directly the script:
+> ./reporting.py
 
 You can find an example of output in output.txt
 
--- reporting.py
+++ reporting.py
@@ -1,72 +1,106 @@
-#!/bin/env python2.7
+#!/usr/bin/env python2.7
 import datetime
 import psycopg2
 
 
-def get_authors():
-    """Return authors ordered by total views"""
-    db = psycopg2.connect("dbname=news")
+# database to use
+DBNAME = "dbname=news"
+
+
+def db_connect():
+    """ Creates and returns a connection to the database defined by DBNAME,
+       as well as a cursor for the database.
+
+        Returns:
+            db, c - a tuple. The first element is a connection to the database.
+                    The second element is a cursor for the database.
+    """
+
+    db = psycopg2.connect(DBNAME)
     c = db.cursor()
-    c.execute(
-        "select authors.name, aviews.sum \
-        from (select author, sum(views) \
-            from (select b.author, a.views \
-                from (select substring(path from 10) as slug, \
-                    count(*) as views \
-                from log where path like '/article/%' group by path) as a \
-                join articles as b on a.slug = b.slug) \
-                    as qry group by author) as aviews \
-        join authors on aviews.author = authors.id order by aviews.sum desc")
-    return c.fetchall()
-    db.close()
+    return db, c
 
 
-def get_top_articles():
-    """Return top 3 most viewed articles"""
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute(
-        "select * from \
-             (select b.title, a.views from \
-                 (select substring(path from 10) as slug, count(*) as views \
-                 from log where path like '/article/%' \
-                     group by path order by views desc limit 3) as a \
-                 join articles as b on a.slug = b.slug) \
-         as qry order by views desc")
-    return c.fetchall()
-    db.close()
+def execute_query(query):
+    """ execute_query takes an SQL query as a parameter.
+        Executes the query and returns the results as a list of tuples.
+        args:
+            query - an SQL query statement to be executed.
+
+        returns:
+            A list of tuples containing the results of the query.
+    """
+
+    try:
+        db, c = db_connect()
+        c.execute(query)
+        results = c.fetchall()
+        db.close()
+        return results
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
 
 
-def get_errors():
-    """Return all days with more than 1% request errors"""
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute(
-        "select a.time, b.errors * 100::float / a.total as perc \
-         from \
-             (select time::date, count(*) as total \
-                 from log group by time::date) as a \
-             join \
-             (select time::date, count(*) as errors \
-                 from log where status !~~ '200 OK' group by time::date) as b \
-             on a.time = b.time \
-          where b.errors * 100::float / a.total > 1")
-    return c.fetchall()
-    db.close()
+def print_top_articles():
+    """Prints out the top 3 articles of all time."""
 
-print("HERE'S YOUR REPORT!\n")
+    query = """select b.title, a.views from
+             (select substring(path from 10) as slug, count(*) as views
+             from log where path like '/article/%'
+                 group by path) as a
+             join articles as b on a.slug = b.slug
+             order by a.views desc limit 3"""
 
-print("*** Most readed authors: ***\n")
-results = get_authors()
-for result in results:
-    print(result[0] + ": " + str(result[1]) + " views")
+    results = execute_query(query)
 
-print("\n\n*** Top 3 viewed articles: ***\n")
-results = get_top_articles()
-for result in results:
-    print(result[0] + ": " + str(result[1]) + " views")
+    print("\n\n*** Top 3 viewed articles: ***\n")
+    for title, views in results:
+        print('\"{}\": {} views'.format(title, views))
 
-print("\n\n*** Days with more than 1% request errors: ***\n")
-results = get_errors()
-for result in results:
-    print(str(result[0]) + ": " + "%.2f" % result[1] + "% of errors")
+
+def print_top_authors():
+    """Prints a list of authors ranked by article views."""
+
+    query = """select authors.name, aviews.sum
+        from (select author, sum(views)
+            from (select b.author, a.views
+                from (select substring(path from 10) as slug,
+                    count(*) as views
+                from log where path like '/article/%' group by path) as a
+                join articles as b on a.slug = b.slug)
+                    as qry group by author) as aviews
+        join authors on aviews.author = authors.id order by aviews.sum desc"""
+
+    results = execute_query(query)
+
+    print("\n\n*** Most readed authors: ***\n")
+    for author, views in results:
+        print('\"{}\": {} views'.format(author, views))
+
+
+def print_errors_over_one():
+    """ Prints out the days where more than 1% of logged access requests
+        were errors.
+    """
+
+    query = """select a.time, b.errors * 100::float / a.total as perc
+         from
+             (select time::date, count(*) as total
+                 from log group by time::date) as a
+             join
+             (select time::date, count(*) as errors
+                 from log where status !~~ '200 OK' group by time::date) as b
+             on a.time = b.time
+          where b.errors * 100::float / a.total > 1"""
+    results = execute_query(query)
+
+    print("\n\n*** Days with more than 1% request errors: ***\n")
+    for day, errors in results:
+        print(str(day) + ": " + "%.2f" % errors + "% of errors")
+
+
+if __name__ == '__main__':
+    print("HERE'S YOUR REPORT!")
+    print_top_articles()
+    print_top_authors()
+    print_errors_over_one()
